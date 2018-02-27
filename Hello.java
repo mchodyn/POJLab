dc20f1dbe05eb0a259d34d21bba284fac2a63446
s17606 <s17606@lab5l-15.pjwstk.edu.pl>
2018-02-27 15:07:38 +0100

    da:x :x

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
 class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello");
-	}
+		boolean a=true;
+if(a){
+System.out.println("hi");
+}
+else
+{
+System.out.println("ho");
+}	}
 }
